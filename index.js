3121401e8311788d29212cb27a9317c2a25d5f5c
laizuakram1 <laizuakram98@gmail.com>
2022-02-17 18:59:38 +0600

    total expense and balance

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
 document.getElementById('calculate-btn').addEventListener('click', function(){
+    const totalIncomeInput = document.getElementById('total-income');
+    const totalIncome = totalIncomeInput.value;
+
     const foodInput = document.getElementById('food-input');
     const foodInputField = parseInt(foodInput.value);
 
@@ -9,7 +12,13 @@ document.getElementById('calculate-btn').addEventListener('click', function(){
     const clothesInput = document.getElementById('clothes-input');
     const clothesInputField = parseInt(clothesInput.value);
     const totalInput = foodInputField + rentInputField + clothesInputField;
+    
 
     const totalExpense = document.getElementById('total-expense');
     totalExpense.innerText = totalInput;
+
+    
+
+    const balanceTotal = document.getElementById('balance');
+    balanceTotal.innerText = totalIncome - totalInput;
 })
